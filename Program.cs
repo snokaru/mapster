ca6552acc15e6546efe4574076bf5347addc2459
Romeo Calota <mail@romeocalota.me>
2022-03-09 14:20:02 +0200

    lab_2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Buffers.Binary;
+using System.Buffers.Binary;
+using System.IO.Compression;
 
 // The primary namespace of the application
 namespace Mapster
@@ -12,18 +13,36 @@ namespace Mapster
             // Open the local file using a Stream (https://docs.microsoft.com/en-us/dotnet/api/system.io.stream?view=net-6.0)
             using FileStream fs = new FileStream(@"", FileMode.Open);
 
-            // Allocate enough bytes to hold an 32bit int
+            // Allocate enough bytes to hold a 32bit int
             byte[] buffer = new byte[4];
             // Read from the stream into the buffer
             fs.Read(buffer, 0, buffer.Length);
             // Interpret the bytes as a big-endian 32bit int (https://docs.microsoft.com/en-us/dotnet/api/system.buffers.binary.binaryprimitives?view=net-6.0)
             int headerSize = BinaryPrimitives.ReadInt32BigEndian(buffer.AsSpan());
 
-            // Alternative method of interpreting those bytes
-            // Just reverse the order of the bytes in the buffer
-            Array.Reverse(buffer);
-            // Interpret the bytes as a 32bit little-endian int (https://docs.microsoft.com/en-us/dotnet/api/system.bitconverter?view=net-6.0)
-            int headerSize2 = BitConverter.ToInt32(buffer);
+            // Allocate enough bytes to hold the serialized header (headerBytes)
+            byte[] headerBuffer = new byte[headerSize];
+            // Read the serialized header into the buffer
+            fs.Read(headerBuffer, 0, headerBuffer.Length);
+            // Deserialize the header
+            BlobHeader blobHeader = BlobHeader.Parser.ParseFrom(headerBuffer);
+
+            // Allocate enough bytes to hold the serialized blob (blobHeader.Datasize)
+            byte[] blobBuffer = new byte[blobHeader.Datasize];
+            // Read the serialized blob into the buffer
+            fs.Read(blobBuffer, 0, blobBuffer.Length);
+            // Deserialize the blob
+            Blob blob = Blob.Parser.ParseFrom(blobBuffer);
+
+            // The blob contains ZLib compressed data, hold reference to those bytes
+            var compressedData = blob.ZlibData.Span;
+            // Create a MemoryStream that uses the compressed bytes as a source
+            MemoryStream ms = new MemoryStream(compressedData.ToArray());
+            // Overlay a ZLib decompression stream on top of the memory stream do decompress the
+            // data on the fly
+            ZLibStream zlibStream = new ZLibStream(ms, CompressionMode.Decompress);
+            // Deserialze a HeaderBlock from the compressed stream
+            HeaderBlock headerBlock = HeaderBlock.Parser.ParseFrom(zlibStream);
         }
     }
 }
