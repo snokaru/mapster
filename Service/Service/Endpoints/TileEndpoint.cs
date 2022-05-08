e5aa32c94aa47b644b585e23729745c5ee018814
Romeo Calota <mail@romeocalota.me>
2022-05-08 14:53:09 +0300

    Fix up Service

--- Service/Service/Endpoints/TileEndpoint.cs
+++ Service/Service/Endpoints/TileEndpoint.cs
@@ -1,24 +1,24 @@
-using Mapster.Common;
+using Mapster.Common.MemoryMappedTypes;
 using Mapster.Rendering;
 using SixLabors.ImageSharp;
-using Newtonsoft.Json.Bson;
-using System.IO.MemoryMappedFiles;
 
 namespace Mapster.Service.Endpoints;
 
 internal class TileEndpoint : IDisposable
 {
+    private readonly DataFile _mapData;
     private bool _disposedValue;
-    private List<MemoryMappedFile> _mmappedFiles = new List<MemoryMappedFile>();
 
-    Dictionary<int, (long, MemoryMappedFile)> _tileSet = new Dictionary<int, (long, MemoryMappedFile)>();
-
-    public TileEndpoint(string mapDataPath)
+    public TileEndpoint(string mapDataFilePath)
     {
-        foreach (var binaryFile in Directory.EnumerateFiles(mapDataPath))
-        {
-            LoadBinaryData(binaryFile, _tileSet);
-        }
+        _mapData = new DataFile(mapDataFilePath);
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 
     public static void Register(WebApplication app)
@@ -33,117 +33,39 @@ internal class TileEndpoint : IDisposable
                 size = 800;
             }
 
-            var tileIds = TiligSystem.GetTilesForBoundingBox(minLat,
-minLon, maxLat, maxLon);
-
-            context.Response.ContentType = "application/bson";
-            await tileEndpoint.SerializeBson(context.Response.BodyWriter.AsStream(), tileIds, size.Value);
-        }
-    }
-
-    private async Task SerializeBson(Stream outputStream, int[] tileIds, int size)
-    {
-        var responseWriter = new BsonDataWriter(outputStream);
-
-        await responseWriter.WriteStartObjectAsync();
-        {
-            // Used to store encoded image data
-            var imageMemoryStream = new MemoryStream();
-
-            // Write the number of tiles
-            await responseWriter.WritePropertyNameAsync("tileCount");
-            await responseWriter.WriteValueAsync(tileIds.Length);
-
-            await responseWriter.WritePropertyNameAsync("imageData");
-            await responseWriter.WriteStartArrayAsync();
+            var pixelBb = new TileRenderer.BoundingBox
             {
-                for (int i = 0; i < tileIds.Length; ++i)
+                MinX = float.MaxValue,
+                MinY = float.MaxValue,
+                MaxX = float.MinValue,
+                MaxY = float.MinValue
+            };
+            var shapes = new PriorityQueue<BaseShape, int>();
+            tileEndpoint._mapData.ForeachFeature(
+                new BoundingBox(
+                    new Coordinate(minLat, minLon),
+                    new Coordinate(maxLat, maxLon)
+                ),
+                featureData =>
                 {
-                    imageMemoryStream.Position = 0;
-                    await RenderPng(imageMemoryStream, tileIds[i], size, size);
-                    await responseWriter.WriteValueAsync(imageMemoryStream.ToArray());
+                    featureData.Tessellate(ref pixelBb, ref shapes);
+                    return true;
                 }
-            }
-            await responseWriter.WriteEndArrayAsync();
+            );
+
+            context.Response.ContentType = "image/png";
+            await tileEndpoint.RenderPng(context.Response.BodyWriter.AsStream(), pixelBb, shapes, size.Value, size.Value);
         }
-        await responseWriter.WriteEndObjectAsync();
     }
 
-    private async Task RenderPng(Stream outputStream, int tileId, int width, int height)
+    private async Task RenderPng(Stream outputStream, TileRenderer.BoundingBox boundingBox, PriorityQueue<BaseShape, int> shapes, int width, int height)
     {
-        var tileData = new KeyValuePair<int, MapFeature[]>(tileId, ExtractMapFeatures(tileId));
-
         var canvas = await Task.Run(() =>
         {
-            return tileData.Render(width, height);
-        });
+            return shapes.Render(boundingBox, width, height);
+        }).ConfigureAwait(continueOnCapturedContext: false);
 
-        await canvas.SaveAsPngAsync(outputStream);
-    }
-
-    private MapFeature[] ExtractMapFeatures(int tileId)
-    {
-        if (!_tileSet.TryGetValue(tileId, out var featureData))
-        {
-            return new MapFeature[0];
-        }
-
-        var (offset, file) = featureData;
-        using var stream = file.CreateViewStream();
-
-        stream.Position = offset;
-        using var reader = new BinaryReader(stream);
-
-        var featureCount = reader.ReadInt32();
-        var features = new MapFeature[featureCount];
-
-        for (int i = 0; i < featureCount; ++i)
-        {
-            var featureId = reader.ReadInt64();
-            var featureLabel = reader.ReadString();
-            var featureType = (MapFeature.GeometryType)reader.ReadByte();
-            var coordinates = new Coordinate[reader.ReadInt32()];
-            for (int j = 0; j < coordinates.Length; ++j)
-            {
-                coordinates[j] = new Coordinate(reader.ReadDouble(), reader.ReadDouble());
-            }
-
-            var propertyCount = reader.ReadInt32();
-            var properties = new Dictionary<string, string>(propertyCount);
-            for (int j = 0; j < propertyCount; ++j)
-            {
-                properties[reader.ReadString()] = reader.ReadString();
-            }
-
-            features[i] = new MapFeature()
-            {
-                Id = featureId,
-                Label = featureLabel,
-                Type = featureType,
-                Coordinates = coordinates,
-                Properties = properties,
-            };
-        }
-
-        return features;
-    }
-
-    private Dictionary<int, (long, MemoryMappedFile)> LoadBinaryData(string path, Dictionary<int, (long, MemoryMappedFile)> result)
-    {
-        var mmappedFile = MemoryMappedFile.CreateFromFile(path, FileMode.Open);
-        _mmappedFiles.Add(mmappedFile);
-
-        using var stream = mmappedFile.CreateViewStream();
-        using var reader = new BinaryReader(stream);
-
-        var tileCount = reader.ReadInt32();
-
-        for (int i = 0; i < tileCount; ++i)
-        {
-            result[reader.ReadInt32()] = (reader.ReadInt64(), mmappedFile);
-        }
-
-        return result;
+        await canvas.SaveAsPngAsync(outputStream).ConfigureAwait(continueOnCapturedContext: false);
     }
 
     protected virtual void Dispose(bool disposing)
@@ -152,20 +74,10 @@ minLon, maxLat, maxLon);
         {
             if (disposing)
             {
-                foreach (var mmapFile in _mmappedFiles)
-                {
-                    mmapFile.Dispose();
-                }
+                _mapData.Dispose();
             }
 
             _disposedValue = true;
         }
-    }
-
-    public void Dispose()
-    {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
     }
 }
